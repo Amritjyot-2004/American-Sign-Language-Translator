594a524e360673ed217f7adaefff81b3c526a274
Amritjyot Singh <118835220+Amritjyot-2004@users.noreply.github.com>
2025-05-12 00:05:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ async def process_frame():
     return app.response_class(jpeg.tobytes(), mimetype='image/jpeg')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host = 0.0.0.0, port = 5000)
